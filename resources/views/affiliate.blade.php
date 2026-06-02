d38d804e212e2ea76f2174cbc940e71aaf405283
Khamid-v2022 <khamid.webdev@gmail.com>
2026-06-02 13:45:57 +0300

    Updated minnor changed

--- resources/views/affiliate.blade.php
+++ resources/views/affiliate.blade.php
@@ -561,7 +561,11 @@
     const text = document.getElementById(id).textContent;
     navigator.clipboard.writeText(text).then(() => {
       btn.textContent = '✓ Copied!';
-      setTimeout(() => { btn.textContent = '⊕ Copy Swipe'; }, 2000);
+      if(id==='affiliateLink') {
+        setTimeout(() => { btn.textContent = '⊕ Copy Link'; }, 2000);
+      } else {
+        setTimeout(() => { btn.textContent = '⊕ Copy Swipe'; }, 2000);
+    }
     });
   }
 

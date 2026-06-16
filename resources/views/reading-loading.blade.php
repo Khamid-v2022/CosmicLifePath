951f44b216b382984be31bdda550e99d3ccb95bd
Khamid-v2022 <khamid.webdev@gmail.com>
2026-06-16 23:11:33 +0300

    Updated the embed video code

--- resources/views/reading-loading.blade.php
+++ resources/views/reading-loading.blade.php
@@ -27,7 +27,7 @@
           <p class="section-label mb-2">Meet Celestra Vonn</p>
 
           <div class="cosmic-video-frame video-thumbnail-frame position-relative mt-3">
-            <div id="vidalytics_embed_wJVfmwAQT5Xg8A1J" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
+            <div id="vidalytics_embed_3iGiV97kgugwhHYn" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
           </div>
 
           <!-- <p class="loading-subtext mt-3">Watch this before viewing your results — and discover how Celestra can help you.</p> -->
@@ -75,6 +75,16 @@
     // };
 
     function loadVidalyticsVideo() {
+        // (function (v, i, d, a, l, y, t, c, s) {
+        //     y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
+        //     if (!vsl){vsl=function(u,cb){
+        //         if(t){cb();return;}s=i.createElement("script");s.type="text/javascript";s.async=1;s.src=u;
+        //         if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.readyState=="complete"){s.onreadystatechange=null;vlf=1;cb();}};}else{s.onload=function(){vlf=1;cb();};}
+        //         i.getElementsByTagName("head")[0].appendChild(s);
+        //     };}
+        //     vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
+        // })(window, document, 'Vidalytics', 'vidalytics_embed_wJVfmwAQT5Xg8A1J', 'https://fast.vidalytics.com/embeds/x4wjDf3w/wJVfmwAQT5Xg8A1J/');
+
         (function (v, i, d, a, l, y, t, c, s) {
             y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
             if (!vsl){vsl=function(u,cb){
@@ -83,7 +93,8 @@
                 i.getElementsByTagName("head")[0].appendChild(s);
             };}
             vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
-        })(window, document, 'Vidalytics', 'vidalytics_embed_wJVfmwAQT5Xg8A1J', 'https://fast.vidalytics.com/embeds/x4wjDf3w/wJVfmwAQT5Xg8A1J/');
+        })(window, document, 'Vidalytics', 'vidalytics_embed_3iGiV97kgugwhHYn', 'https://fast.vidalytics.com/embeds/x4wjDf3w/3iGiV97kgugwhHYn/');
+
     }
     
     const loadingStage = document.getElementById('loadingStage');
